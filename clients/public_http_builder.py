02ed4ff38e512c57070a6797e7af35a9ee6dd6a9
Eugene Kotov <eug01001@gmail.com>
2026-05-12 10:58:20 +0300

    fix

--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,14 +1,7 @@
 from httpx import Client
 
-from typing import TypedDict
 
 
-class AuthenticationUsersDict(TypedDict):
-        phoneNumberOrEmail: str
-        password: str
-
-
-HOST = "https://api.prod.booking.ktsf.ru"
 
 
 def get_public_http_client() -> Client: # без заголовков
