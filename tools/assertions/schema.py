69f6daeea882d5e1ce5a105680d38c0b13f497aa
Eugene Kotov <eug01001@gmail.com>
2026-05-12 12:19:57 +0300

    add assertion

---
+++ tools/assertions/schema.py
@@ -0,0 +1,18 @@
+from typing import Any
+from jsonschema import validate
+from jsonschema.validators import Draft202012Validator
+
+
+def validate_json_schema(instance: Any, schema: dict) -> None:
+    """
+    Проверрка json объект (instance) на соответствие json схеме (schema)
+
+    :param instance: JSON объект
+    :param schema: JSON схема
+    :raise: jsonschema.exceptions.ValidationError: если instance объект не соответствует schema схеме
+    """
+    validate(
+        schema=schema,
+        instance=instance,
+        format_checker=Draft202012Validator.FORMAT_CHECKER
+    )
