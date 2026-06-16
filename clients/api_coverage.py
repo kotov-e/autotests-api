957fa723f33f2ae729d21688f47f1b33c6f4d7f4
Eugene Kotov <eug01001@gmail.com>
2026-06-16 11:59:52 +0300

    add swagger-coverage-tool ci #1

--- clients/api_coverage.py
+++ clients/api_coverage.py
@@ -2,14 +2,3 @@ from swagger_coverage_tool import SwaggerCoverageTracker
 import httpx
 
 tracker = SwaggerCoverageTracker(service="api-course")
-
-
-#account_tracker = SwaggerCoverageTracker(service="api-account")
-
-# @tracker.track_coverage_httpx("/api/v1/users/{user_id}")
-# def get_user(user_id: str) -> httpx.Response:
-#     return httpx.get(f"http://localhost:8000/api/v1/users/{user_id}")
-
-# @account_tracker.track_coverage_httpx("/api/v1/users")
-# def create_user() -> httpx.Response:
-#     return httpx.post("http://localhost:8000/api/v1/users")
